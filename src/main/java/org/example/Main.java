fe5e742aa9626eda727ec6dce7a6891eeff1010a
SaurabhS120 <saurabh.sonar120@gmail.com>
2023-04-02 22:14:27 +0530

    Log after update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,34 @@ import org.example.logger.command_logger.CommandLogger;
 
 public class Main {
     public static void main(String[] args) {
-        new LogMachine(new CommandLogger()).logAll(new Logable[]{
-                new ParentLogable(
-                        "Weather list",
-                        new MultilineChildLogable(new ChildLogable[]{
-                                new Weather("Mumbai", 30),
-                                new Weather("Pune", 40),
-                                new Weather("Bangalore", 12),
-                                new Weather("Delhi", 25),
-                        })
-                )
-        });
+        Weather mumbaiWeather = new Weather("Mumbai", 30);
+        Weather puneWeather = new Weather("Pune", 40);
+        Weather bangloreWeather = new Weather("Bangalore", 12);
+        Weather delhiWeather = new Weather("Delhi", 25);
+        LogMachine weatherLogMachine = new LogMachine(
+                new CommandLogger(),
+                new Logable[]{
+                        new ParentLogable(
+                                "Weather list",
+                                new MultilineChildLogable(new ChildLogable[]{
+                                        mumbaiWeather,
+                                        puneWeather,
+                                        bangloreWeather,
+                                        delhiWeather,
+                                })
+
+                        )}
+        );
+        weatherLogMachine.logAll();
+        mumbaiWeather.temp = 34;
+        puneWeather.temp = 24;
+        bangloreWeather.temp = 51;
+        delhiWeather.temp = 13;
+        weatherLogMachine.logAll();
+        mumbaiWeather.temp = 42;
+        puneWeather.temp = 23;
+        bangloreWeather.temp = 2;
+        delhiWeather.temp = 14;
+        weatherLogMachine.logAll();
     }
 }
--- src/main/java/org/example/logger/LogMachine.java
+++ src/main/java/org/example/logger/LogMachine.java
@@ -2,13 +2,24 @@ package org.example.logger;
 
 public class LogMachine {
     private Logger logger;
+    private Logable[] logables;
 
     public LogMachine(Logger logger) {
         this.logger = logger;
+        this.logables = new Logable[]{};
+    }
+
+    public LogMachine(Logger logger, Logable[] logables) {
+        this.logger = logger;
+        this.logables = logables;
     }
 
     public void log(Logable logable) {
         logger.log(logable);
+    }
+
+    public void logAll() {
+        logAll(logables);
     }
 
     public void logAll(Logable[] logables) {
