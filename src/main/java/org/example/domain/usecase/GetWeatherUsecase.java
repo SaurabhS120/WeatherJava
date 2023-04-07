ef72bf7656efad83746b30a5ca42bc966b03f836
SaurabhS120 <saurabh.sonar120@gmail.com>
2023-04-07 11:41:54 +0530

    DI created

--- src/main/java/org/example/domain/usecase/GetWeatherUsecase.java
+++ src/main/java/org/example/domain/usecase/GetWeatherUsecase.java
@@ -6,7 +6,7 @@ import org.example.domain.repo.WeatherRepo;
 public class GetWeatherUsecase implements BaseUsecase<WeatherModel[], GetWeatherUsecaseParams> {
     WeatherRepo repo;
 
-    GetWeatherUsecase(WeatherRepo repo) {
+    public GetWeatherUsecase(WeatherRepo repo) {
         this.repo = repo;
     }
 
