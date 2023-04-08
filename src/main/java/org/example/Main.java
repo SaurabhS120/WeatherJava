df08d951c7788f9f3e327f195ac8e3c2eaf535ab
SaurabhS120 <saurabh.sonar120@gmail.com>
2023-04-08 23:10:54 +0530

    Dummy main code removed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,15 @@ package org.example;
 import org.example.domain.model.WeatherModel;
 import org.example.domain.usecase.GetWeatherUsecase;
 import org.example.domain.usecase.GetWeatherUsecaseParams;
-import org.example.logger.*;
+import org.example.logger.LogMachine;
+import org.example.logger.Logable;
+import org.example.logger.ParentLogable;
 import org.example.logger.command_logger.CommandLogger;
 
 public class Main {
-    static DI di = new DI(RepoModes.OPEN_WEATHER_REPO);
-    public static void main(String[] args) {
-        Weather mumbaiWeather = new Weather("Mumbai", 30);
-        Weather puneWeather = new Weather("Pune", 40);
-        Weather bangloreWeather = new Weather("Bangalore", 12);
-        Weather delhiWeather = new Weather("Delhi", 25);
+    static DI di = new DI(RepoModes.DUMMY_REPO);
+
+    static void weatherPrint() {
         GetWeatherUsecase getWeatherUsecase = di.getWeatherUsecase();
         WeatherModel[] weatherModels = getWeatherUsecase.invoke(new GetWeatherUsecaseParams());
         LogMachine weatherLogMachine = new LogMachine(
@@ -24,30 +23,15 @@ public class Main {
 
 
                         ),
-                        new ParentLogable(
-                                "Weather list",
-                                new MultilineChildLogable(new ChildLogable[]{
-                                        mumbaiWeather,
-                                        puneWeather,
-                                        bangloreWeather,
-                                        delhiWeather,
-                                })
-
-
-                        )
                 }
         );
         weatherLogMachine.logAll();
-        mumbaiWeather.temp = 34;
-        puneWeather.temp = 24;
-        bangloreWeather.temp = 51;
-        delhiWeather.temp = 13;
-        weatherLogMachine.logAll();
-        mumbaiWeather.temp = 42;
-        puneWeather.temp = 23;
-        bangloreWeather.temp = 2;
-        delhiWeather.temp = 14;
-        weatherLogMachine.logAll();
+    }
+
+    public static void main(String[] args) {
+        weatherPrint();
+        di = new DI(RepoModes.OPEN_WEATHER_REPO);
+        weatherPrint();
     }
 }
 
